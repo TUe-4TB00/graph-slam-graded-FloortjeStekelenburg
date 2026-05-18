3d942524ca27159539ffb2cb09aa642e10eb4359
Floortje Stekelenburg <f.c.m.stekelenburg@student.tue.nl>
2026-05-18 22:04:36 +0200

    add landmark

--- src/add_landmark_measurement.py
+++ src/add_landmark_measurement.py
@@ -9,7 +9,9 @@ MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (
 
 def add_landmark_measurement(graph, initial_estimate, result):
     # Determine the correct rotation (bearing) and distance from X(4) to L(2) 
-    # rotation = 
-    # distance = 
-    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromDegrees(rotation), distance, MEASUREMENT_NOISE))
+    rotation = math.pi/2 - math.acos(math.sqrt(2)/(math.sqrt((2-math.sqrt(2))**2 + 2)))
+    rotation_deg = math.degrees(rotation)
+
+    distance = math.sqrt((2-math.sqrt(2))**2 + 2)
+    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromDegrees(rotation_deg), distance, MEASUREMENT_NOISE))
     return graph
