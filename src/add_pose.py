9309f9ec7288dc7c7689c045e68e43d51e13da0f
Floortje Stekelenburg <f.c.m.stekelenburg@student.tue.nl>
2026-05-18 21:36:24 +0200

    add_pose5

--- src/add_pose.py
+++ src/add_pose.py
@@ -10,17 +10,14 @@ MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (
 
 def add_pose(graph, initial_estimate):
 
-    odometry = gtsam.Pose2(
-        math.sqrt(2),
-        math.sqrt(2),
-        math.radians(90)
+    odometry = gtsam.Pose2(math.sqrt(2), math.sqrt(2), math.pi / 2)
+
+    graph.add(
+        gtsam.BetweenFactorPose2(X(3), X(4), odometry, ODOMETRY_NOISE)
     )
 
-    graph.add(gtsam.BetweenFactorPose2(X(3), X(4), odometry, ODOMETRY_NOISE))
-
-
     pose3 = initial_estimate.atPose2(X(3))
-    pose4 = pose3.compose(odometry)
+    pose4 = odometry.compose(pose3)
 
     initial_estimate.insert(X(4), pose4)
     
