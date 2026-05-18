6c74e590dd5eb022b11df8297d2bece75a9912ca
Floortje Stekelenburg <f.c.m.stekelenburg@student.tue.nl>
2026-05-18 21:15:43 +0200

    add pose3

--- src/add_pose.py
+++ src/add_pose.py
@@ -25,8 +25,6 @@ def add_pose(graph, initial_estimate):
     initial_estimate.insert(X(4), pose4)
     
 
-
-
     # TODO: Add the odometry factor between X(4) and X(5) to the graph (BetweenFactorPose2)
     # TODO: Based on the odometry, find the initial estimate for the pose of X(5) and add it to the graph
     
